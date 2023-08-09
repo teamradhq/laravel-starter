bf09e1373edcdb6bcd928a45c55198191a9879b4
Paul Beynon <paul@beynon.app>
2023-08-09 15:27:43 +1000

    FEATURE: Setup livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,23 @@
+@php
+    $classes = Arr::join([
+        'mx-auto',
+        'my-4',
+        'overflow-hidden',
+        'shadow-sm',
+        'sm:rounded-lg',
+        'text-lg',
+        'text-center',
+        'w-12',
+        'h-12',
+        'flex',
+        'items-center',
+        'justify-center',
+    ], ' ');
+@endphp
+
+<div>
+    <div class="{{$classes}} bg-gray-100 dark:bg-gray-900">
+        {{$count}}
+    </div>
+    <button  class="{{$classes}} bg-gray-300 dark:bg-gray-700 hover:bg-gray-400 hover:dark:bg-gray-600" wire:click="increment" >+</button>
+</div>
