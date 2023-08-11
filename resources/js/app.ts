055b187148ee122bc276548bcbd2fe19fb91bf73
Paul Beynon <paul@beynon.app>
2023-08-11 14:27:19 +1000

    DEVELOP: Livewire live reload

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,7 +2,9 @@ import './global.d.ts';
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { livewire_hot_reload } from 'virtual:livewire-hot-reload';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+livewire_hot_reload();
